aaeb18a9fdadf0fc0e115a9c14df18c79442d31c
ANI1KET <aniketrouniyar12@gmail.com>
2024-02-01 18:58:46 +0530

    TASK_MANAGER

---
+++ app/context/globalProvider.js
@@ -0,0 +1,120 @@
+"use client";
+import React, { createContext, useState, useContext } from "react";
+import themes from "./themes";
+import axios from "axios";
+import toast from "react-hot-toast";
+import { useUser } from "@clerk/nextjs";
+
+export const GlobalContext = createContext();
+export const GlobalUpdateContext = createContext();
+
+export const GlobalProvider = ({ children }) => {
+  const { user } = useUser();
+
+  const [selectedTheme, setSelectedTheme] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
+  const [modal, setModal] = useState(false);
+  const [collapsed, setCollapsed] = useState(true);
+
+  const [tasks, setTasks] = useState([]);
+
+  const theme = themes[selectedTheme];
+
+  const openModal = () => {
+    setModal(true);
+  };
+
+  const closeModal = () => {
+    setModal(false);
+  };
+
+  const collapseMenu = () => {
+    setCollapsed(!collapsed);
+  };
+
+  const allTasks = async () => {
+    setIsLoading(true);
+    try {
+      const res = await axios.get("/api/tasks");
+
+      const sorted = res.data.sort((a, b) => {
+        return (
+          new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
+        );
+      });
+
+      setTasks(sorted);
+
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const deleteTask = async (id) => {
+    try {
+      const res = await axios.delete(`/api/tasks/${id}`);
+
+      setTasks(tasks.map(obj => { if (obj.id !== res.data.id) return obj }).filter(obj => obj))
+
+      toast.success("Task deleted");
+    } catch (error) {
+      console.log(error);
+      toast.error("Something went wrong");
+    }
+  };
+
+  const updateTask = async (task) => {
+    try {
+      const res = await axios.put(`/api/tasks`, task);
+
+      setTasks(tasks.map(obj => obj.id === res.data.id ? res.data : obj));
+
+      toast.success("Task updated");
+    } catch (error) {
+      console.log(error);
+      toast.error("Something went wrong");
+    }
+  };
+
+  const updateTheme = async (theme) => {
+    setSelectedTheme(theme === "Dark" ? 0 : 1)
+  }
+
+  const completedTasks = tasks.filter((task) => task.isCompleted === true);
+  const importantTasks = tasks.filter((task) => task.isImportant === true);
+  const incompleteTasks = tasks.filter((task) => task.isCompleted === false);
+
+  React.useEffect(() => {
+    if (user) allTasks();
+  }, [user]);
+
+  return (
+    <GlobalContext.Provider
+      value={{
+        theme,
+        tasks,
+        deleteTask,
+        isLoading,
+        completedTasks,
+        importantTasks,
+        incompleteTasks,
+        updateTask,
+        modal,
+        openModal,
+        closeModal,
+        allTasks,
+        collapsed,
+        collapseMenu,
+        updateTheme,
+      }}
+    >
+      <GlobalUpdateContext.Provider value={{}}>
+        {children}
+      </GlobalUpdateContext.Provider>
+    </GlobalContext.Provider>
+  );
+};
+
+export const useGlobalState = () => useContext(GlobalContext);
+export const useGlobalUpdate = () => useContext(GlobalUpdateContext);
